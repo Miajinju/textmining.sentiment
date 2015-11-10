7efb24352a82d6ebcac2ecbc5f5a0638e5437a81
Miajinju <miabea44@gmai.com>
2015-11-10 10:48:31 +0100

    add codes

--- tm/playground.tm/twitter.tm/interaction.tm/03.R
+++ tm/playground.tm/twitter.tm/interaction.tm/03.R
@@ -1,46 +1,52 @@
 # Script for graphing Twitter friends/followers
-# by Kai Heinrich (kai.heinrich@mailbox.tu-dresden.de) 
+
+#authotization for the twitter 
+
+
+api_key ="NnsMrGv2CEF8g71LqVOiHdXeg"
+api_secret ="OCt39dwLhpt9WFKRq3mD9k4o2zSRagLr3GZVBjBAFzAX09pe5I"
+access_token ="142569552-AHcBcvwBckHbQOiWiwq99iXErWHJlQ7QRphyTHqz"
+access_token_secret ="mftLRi6SDv0fJ09cePgw4ae5rAJNgY3KB7rW8K68mWXa0"
+
+options(httr_oauth_cache = T)
+setup_twitter_oauth(api_key,api_secret,access_token,access_token_secret)
+
 
 # load the required packages
 
 library("twitteR")
 library("igraph")
 
-# HINT: In order for the tkplot() function to work on mac you need to install 
-#       the TCL/TK build for X11 
-#       (get it here: http://cran.us.r-project.org/bin/macosx/tools/)
-#
-# Get User Information with twitteR function getUSer(), 
-#  instead of using ur name you can do this with any other username as well 
 
-start<-getUser("YOUR_USERNAME") 
+# Get User Information with twitteR function getUSer(), 
+
+viki = getUser("viki") 
 
 # Get Friends and Follower names with first fetching IDs (getFollowerIDs(),getFriendIDs()) 
-and then looking up the names (lookupUsers()) 
+  #and then looking up the names (lookupUsers()) 
 
-friends.object<-lookupUsers(start$getFriendIDs())
-follower.object<-lookupUsers(start$getFollowerIDs())
+friends.viki = lookupUsers(viki$getFriendIDs())
+follower.viki = lookupUsers(viki$getFollowerIDs())
 
-# Retrieve the names of your friends and followers from the friend
-# and follower objects. You can limit the number of friends and followers by adjusting the 
-# size of the selected data with [1:n], where n is the number of followers/friends 
-# that you want to visualize. If you do not put in the expression the maximum number of 
+# Retrieve the names of your friends and followers from the friend and follower objects. 
+# You can limit the number of friends and followers with [1:n]
 # friends and/or followers will be visualized.
 
-n<-20 
-friends <- sapply(friends.object[1:n],name)
-followers <- sapply(followers.object[1:n],name)
+n = 150
+friends = sapply(friends.object[1:n],name)
+followers = sapply(followers.object[1:n],name)
 
 # Create a data frame that relates friends and followers to you for expression in the graph
-relations <- merge(data.frame(User='YOUR_NAME', Follower=friends), 
-                   data.frame(User=followers, Follower='YOUR_NAME'), all=T)
+relations = merge(data.frame(User='viki', Follower=friends), 
+                   data.frame(User=followers, Follower='viki'), all=T)
 
 # Create graph from relations.
-g <- graph.data.frame(relations, directed = T)
+g = graph.data.frame(relations, directed = T)
 
 # Assign labels to the graph (=people's names)
-V(g)$label <- V(g)$name
+V(g)$label = V(g)$name
 
-# Plot the graph using plot() or tkplot(). Remember the HINT at the 
-# beginning if you are using MAC OS/X
+# Plot the graph using plot() or tkplot(). 
 tkplot(g)
+
+
--- tm/playground.tm/twitter.tm/interaction.tm/04.R
+++ tm/playground.tm/twitter.tm/interaction.tm/04.R
@@ -1,18 +1,37 @@
+#authotization for the twitter 
+
+
+api_key ="NnsMrGv2CEF8g71LqVOiHdXeg"
+api_secret ="OCt39dwLhpt9WFKRq3mD9k4o2zSRagLr3GZVBjBAFzAX09pe5I"
+access_token ="142569552-AHcBcvwBckHbQOiWiwq99iXErWHJlQ7QRphyTHqz"
+access_token_secret ="mftLRi6SDv0fJ09cePgw4ae5rAJNgY3KB7rW8K68mWXa0"
+
+options(httr_oauth_cache = T)
+setup_twitter_oauth(api_key,api_secret,access_token,access_token_secret)
+
+# load the package
 require("RCurl")
-top.100 <- getURL("http://twittercounter.com/pages/100")
+top.100 = getURL("http://twittercounter.com/pages/100")
+devtools::install_github("lmullen/gender")
+require("gender")
+
 
 # split into lines
-top.100 <- unlist(strsplit(top.100, "\n"))
+top.100 = unlist(strsplit(top.100, "\n"))
+head(top.100)
+
 # Get only those lines with an @
-top.100 <- top.100[sapply(top.100, grepl, pattern="@")]
+top.100 = top.100[sapply(top.100, grepl, pattern="@")]
+head(top.100)
 
 # Grep out anchored usernames: <a ...>@username</a>
-top.100 <- gsub(".*>@(.+)<.*", "\\1", top.100)[2:101]
+top.100 = gsub(".*>@(.+)<.*", "\\1", top.100)[2:101]
 head(top.100)
-# [1] "katyperry"  "justinbieber"  "BarackObama"  ...
 
-# Try to sample 3000 followers for a user:
-username$getFollowers(n=3000)
+
+# Try to sample 300 followers for a user:
+username$getFollowers(n=300)
+
 # Error in twFromJSON(out) :
 #  Error: Malformed response from server, was not JSON.
 # The most likely cause of this error is Twitter returning
@@ -20,9 +39,6 @@ username$getFollowers(n=3000)
 # the only remedy is to wait long enough for the offending
 # character to disappear from searches.
 
-devtools::install_github("lmullen/gender")
-require("gender")
 gender("ben")
-#   name proportion_male proportion_female gender
-# 1  ben          0.9962            0.0038   male
+
 
