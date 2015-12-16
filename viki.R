10c919f89a7960f9852730c48d333414064eddd0
Miajinju <miabea44@gmai.com>
2015-12-16 14:58:49 +0100

    something change..? understand nothing

--- viki.R
+++ viki.R
@@ -20,3 +20,4 @@ str(hmac)
 
 hmac("c80f43eaa234dd4207197014562909bfafe0deb31ad27d4e96dafc89b81f59b1ad659968
 ", "/v4/movies.json?sort=views&app=100485a&t=1356482778", algo="sha1")
+ # [1] "375fc95f773d638453106dbb7f15f53897a9b36e"
